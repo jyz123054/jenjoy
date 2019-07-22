316eac58bb174de32892f4d70e80aa72a0994775
jyz <353515101@qq.com>
2019-07-22 21:51:14 +0800

    J0201Mod

--- src/main/java/com/test/springboot/controller/UserController.java
+++ src/main/java/com/test/springboot/controller/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
 //    	log.debug("Hello Jyz. debug log.......");
 //    	log.trace("Hello Jyz. trace log.......");
 //    	readProperties();
-        return "Hello git. v1.0.1";
+        return "Hello git. v1.0.1    J0201mod";
     }
 	
     @RequestMapping("/index")
